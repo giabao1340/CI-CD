ffe319a4eca72a4685fcded66dee7189f0ab7b2a
giabao1340 <luonggiabao060904@gmail.com>
2026-05-21 12:25:32 +0700

    Fix

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create A Next App</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
